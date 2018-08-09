4b7b1da98b121bdbfac447a9b5b95f0e50bfed34
Daniel <cpt.capy@hotmail.de>
2018-08-09 14:35:12 +0200

    added template cypher selection and did small fixes in html templates

--- neo4jpoc/src/main/java/springframework/controller/HomeController.java
+++ neo4jpoc/src/main/java/springframework/controller/HomeController.java
@@ -17,12 +17,21 @@ public class HomeController {
     private static final String INDEX = "index";
     private static final String COMMANDS = "commands";
 
+    /**
+     *
+     * @return
+     */
     @RequestMapping("/" + INDEX)
     public String getIndexPage() {
         logger.info("Loading index page.");
         return INDEX;
     }
 
+    /**
+     *
+     * @param model
+     * @return
+     */
     @RequestMapping("/" + COMMANDS)
     public String getAllCypherCommands(Model model) {
         logger.info("Loading commands overview page.");
@@ -33,7 +42,13 @@ public class HomeController {
         cmdList.add("/allNodes/label/{label}");
         cmdList.add("/createRelation/title1/{title1}/title2/{title2}/relation/{relation}");
 
-        model.addAttribute(COMMANDS, cmdList);
+        try {
+            if (model != null) {
+                model.addAttribute(COMMANDS, cmdList);
+            }
+        } catch (NullPointerException npe) {
+            logger.error("Could not add to Model {}", model, npe);
+        }
 
         return COMMANDS;
     }
